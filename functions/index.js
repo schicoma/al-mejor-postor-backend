431e462dd1d54d72962b84c3a35174563fba9c40
Sebastian Chicoma <schicoma@bytesw.com>
2019-09-02 16:46:00 -0500

    Actualizando funciones

--- functions/index.js
+++ functions/index.js
@@ -13,50 +13,43 @@ let transporter = nodemailer.createTransport({
     }
 });
 
-exports.sendMail = functions.https.onRequest((request, response) => {
-    cors(request, response, () => {
-        const to = 'sebastianchicoma97@gmail.com';
-        const mailOptions = {
-            from: '"Al mejor postor" <info@almejorpostor.com>',
-            to: to,
-            subject: 'Correo de activación',
-            html: '<b>Correo de activación</b>'
-                + '<br/>'
-                + '<span>Hola Sebastian, sigue el enlace para poder activar su cuenta:</span>'
-                + '<br />'
-                + '<a href="http://www.google.com.pe">http://www.google.com.pe</a>'
-        };
+// Función que se ejecutará al crear a un usuario, enviará el correo de activación
+exports.onCreateUsers = functions.firestore.document('usuarios/{identificador}').onCreate((snap, context) => {
+    const to = context.params.identificador;
 
-        return transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return response.send(error.toString());
-            }
+    const newUser = snap.data();
+    const token = newUser['token'];
 
-            return response.send(JSON.stringify(info));
-        });
+    const mailOptions = {
+        from: '"Al mejor postor" <info@almejorpostor.com>',
+        to: to,
+        subject: 'Correo de activación',
+        html: '<b>Correo de activación</b>'
+            + '<br/>'
+            + '<span>Hola ' + newUser['nombres'] + ', sigue el enlace para poder activar su cuenta:</span>'
+            + '<br />'
+            + '<a href="https://us-central1-angular-crud-firebase-9d4e9.cloudfunctions.net/verifyAccount?token=' + token + '">'
+            + 'https://us-central1-angular-crud-firebase-9d4e9.cloudfunctions.net/verifyAccount?token=' + token + '</a>'
+    };
 
-    });
-});
+    console.log("preparando correo " + JSON.stringify(mailOptions));
 
-exports.hello = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('usuarios').doc('fchicoma01@gmail.com').set({
-        'nombres': "Fernando Luis Enrique",
-        'email': 'fchicoma01@gmail.com',
-        'estado': 'PE'
-    }).then(writeResult => {
-        // write is complete here
-        response.redirect('http://localhost:5000/login');
+    return transporter.sendMail(mailOptions).then((info) => {
+        console.log("Correo enviado correctamente");
         return true;
     }).catch(error => {
-        console.log(error);
+        console.log("Error al enviar correo");
     });
 });
 
+//Servicio para verificar la cuenta del usuario
 exports.verifyAccount = functions.https.onRequest((request, response) => {
+    const token = request.query.token;
+
     let usuarios = admin.firestore().collection("usuarios");
 
     // Create a query against the collection.
-    let query = usuarios.where("email", "==", "fchicoma01@gmail.com")
+    let query = usuarios.where("token", "==", token);
 
     return query.get().then(value => {
         if (!value.empty) {
@@ -85,34 +78,20 @@ exports.verifyAccount = functions.https.onRequest((request, response) => {
 });
 
 async function updateUsers(user) {
-    //await admin.auth().updateUser(user.email, { emailVerified: true });
+    await admin.auth().updateUser(user.uid, { emailVerified: true });
     await admin.firestore().collection("usuarios").doc(user.email).update({ 'estado': 'OK' });
 }
 
-
-// Función que se ejecutará al crear a un usuario
-exports.onCreateUsers = functions.firestore.document('usuarios/{identificador}')
-    .onCreate((snap, context) => {
-        const newUser = snap.data();
-        const to = context.params.identificador;
-        console.log('to:' + to);
-        console.log('newUser:' + JSON.stringify(newUser));
-        const mailOptions = {
-            from: '"Al mejor postor" <info@almejorpostor.com>',
-            to: to,
-            subject: 'Correo de activación',
-            html: '<b>Correo de activación</b>'
-                + '<br/>'
-                + '<span>Hola ' + newUser['nombres'] + ', sigue el enlace para poder activar su cuenta:</span>'
-                + '<br />'
-                + '<a href="http://www.google.com.pe">http://www.google.com.pe</a>'
-        };
-        console.log("preparando correo " + JSON.stringify(mailOptions));
-
-        return transporter.sendMail(mailOptions).then((info) => {
-            console.log("Correo enviado correctamente");
-            return true;
-        }).catch(error => {
-            console.log("Error al enviar correo");
-        });
+exports.hello = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('usuarios').doc('fchicoma01@gmail.com').set({
+        'nombres': "Fernando Luis Enrique",
+        'email': 'fchicoma01@gmail.com',
+        'estado': 'PE'
+    }).then(writeResult => {
+        // write is complete here
+        response.redirect('http://localhost:5000/login');
+        return true;
+    }).catch(error => {
+        console.log(error);
     });
+});
